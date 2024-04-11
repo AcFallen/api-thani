9e9ee1c8ee8c8c8efea70373dbbb38cec446d388
Roberto Carlos Apaza Cornejo <r.apaza.cornejo@gmail.com>
2024-04-11 01:33:14 -0500

    Añadir middleware para manejar solicitudes JSON en index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import express from 'express'
+import * as Rutas from './router/index.js'
 
 const app = express()
+
+app.use(express.json())
+
+
+app.use(Rutas.usuarioRouter)
 
 app.listen(3000)
 

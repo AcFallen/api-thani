1b39a86935dc5a9810a55942972c50ec914b96e7
Roberto <r.apaza.cornejo@gmail.com>
2024-04-14 18:14:24 -0500

    Añadir middleware para habilitar CORS en index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import express from 'express'
+import cors from 'cors'
 import * as Rutas from './router/index.js'
 
 const app = express()
 
+app.use(cors())
+
 app.use(express.json())
+
 
 
 app.use(Rutas.usuarioRouter)

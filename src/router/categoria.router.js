830a8994e1fb1354a3c78832368274a0793bb540
Roberto Carlos Apaza Cornejo <r.apaza.cornejo@gmail.com>
2024-04-15 00:47:27 -0500

    Actualizar rutas de categoría para incluir validación de token y permisos de administrador

--- src/router/categoria.router.js
+++ src/router/categoria.router.js
@@ -1,10 +1,11 @@
 import {Router} from 'express'
 import AsyncHandler from 'express-async-handler'
 import {crearCategoria,eliminarCategoria , listarCategorias} from '../controllers/categoria.controller.js'
+import {validarToken, validarAdmin} from '../validarToken.js'
 
 export const categoriaRouter = Router();
 
 categoriaRouter.post('/crear-categoria',AsyncHandler(crearCategoria));
-categoriaRouter.get('/listar-categorias',AsyncHandler(listarCategorias));
+categoriaRouter.get('/listar-categorias', validarToken, validarAdmin, AsyncHandler(listarCategorias));
 categoriaRouter.delete('/eliminar-categoria/:id',AsyncHandler(eliminarCategoria));
 

2708ee2d53f5d6cede68f1dce94fc2c79e35e711
Roberto <r.apaza.cornejo@gmail.com>
2024-04-15 08:47:25 -0500

    Actualizar rutas de producto para incluir validación de token y permisos de administrador

--- src/router/producto.router.js
+++ src/router/producto.router.js
@@ -1,12 +1,44 @@
 import { Router } from "express";
 import AsyncHandler from "express-async-handler";
 
-import { crearProducto, eliminarProducto, listarProductos, actualizarProducto , listarUnProducto } from "../controllers/producto.controller.js";
+import {
+  crearProducto,
+  eliminarProducto,
+  listarProductos,
+  actualizarProducto,
+  listarUnProducto,
+  eliminarDetalleProducto,
+} from "../controllers/producto.controller.js";
+
+import { validarToken, validarAdmin } from "../validarToken.js";
 
 export const productoRouter = Router();
 
-productoRouter.post("/crear-producto", AsyncHandler(crearProducto));
-productoRouter.get("/listar-productos", AsyncHandler(listarProductos));
-productoRouter.delete("/eliminar-producto/:id", AsyncHandler(eliminarProducto));
-productoRouter.put("/actualizar-producto/:id", AsyncHandler(actualizarProducto));
+productoRouter.post(
+  "/crear-producto",
+  validarToken,
+  validarAdmin,
+  AsyncHandler(crearProducto)
+);
+productoRouter.delete(
+  "/eliminar-producto/:id",
+  validarToken,
+  validarAdmin,
+  AsyncHandler(eliminarProducto)
+);
+productoRouter.put(
+  "/actualizar-producto/:id",
+  validarToken,
+  validarAdmin,
+  AsyncHandler(actualizarProducto)
+);
+
 productoRouter.get("/listar-un-producto/:id", AsyncHandler(listarUnProducto));
+productoRouter.get("/listar-productos", AsyncHandler(listarProductos));
+
+productoRouter.delete(
+  "/eliminar-detalle-producto/:id/:productoId",
+  validarToken,
+  validarAdmin,
+  AsyncHandler(eliminarDetalleProducto)
+);
--- src/router/usuario.router.js
+++ src/router/usuario.router.js
@@ -1,8 +1,10 @@
 import { Router} from 'express'
 import { registro , login } from '../controllers/usuario.controller.js'
 import expressAsyncHandler from 'express-async-handler'
+import { validarToken , validarAdmin } from '../validarToken.js'
 
 export const usuarioRouter = Router()
 
-usuarioRouter.post('/registro',expressAsyncHandler(registro))
+usuarioRouter.post('/registro', validarToken, validarAdmin , expressAsyncHandler(registro))
+
 usuarioRouter.post('/login',expressAsyncHandler(login))
